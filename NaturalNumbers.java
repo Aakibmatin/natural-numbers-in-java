70e03ad3d85f18307d8017d269d27ee660c3a41b
Aakibmatin <124870240+Aakibmatin@users.noreply.github.com>
2023-09-04 20:27:59 +0530

    Create NaturalNumbers.java

---
+++ NaturalNumbers.java
@@ -0,0 +1,13 @@
+//Program to print first 10 natural numbers.
+public class NaturalNumbers {
+
+	public static void main(String[] args) {
+		int i;
+		System.out.println("The first 10 natural numbers are as follows:");
+		for(i=1;i<=10;i++)
+		{ 
+			System.out.println(i);
+		}	
+	}
+
+}
